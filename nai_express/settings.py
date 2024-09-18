a70b5ae5efb1ea6f944f0a66e7e6028c10093c90
nairafiany <naira.afiany@gmail.com>
2024-09-18 07:05:23 +0700

    Menjalankan collectstatic

--- nai_express/settings.py
+++ nai_express/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'main'
 ]
-
+# test
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
